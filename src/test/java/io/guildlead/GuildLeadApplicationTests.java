6a87c53241d0a30ce12f3384ef9c111c34196ac3
Marcel Uhlemeyer <>
2021-03-14 21:32:19 +0100

    Add spring boot 2.4.3 with gradle

---
+++ src/test/java/io/guildlead/GuildLeadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.guildlead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildLeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
